95d2ffd79e85336a616175c3f14fa10a901e9c7d
anand biniya <you@example.com>
2026-08-26 20:55:23 +0530

    Update Hostillam website

--- src/data/magikMedia.js
+++
@@ -1,6 +0,0 @@
-/**
- * My Magik Place links / metadata only.
- * Product photos come from the host’s uploaded Git images (see imageMap.js / src/Images/).
- * Do not import Cursor-fetched Instagram stills here.
- */
-export const MY_MAGIK_PLACE_INSTAGRAM_URL = 'https://www.instagram.com/mymagikplace/'
